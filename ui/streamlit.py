4049686592011dde8c09d8d9928a8e7e9027d6d9
amstu2 <11323044+amstu2@users.noreply.github.com>
2026-05-07 20:21:22 +1000

    Trigger workflow

--- ui/streamlit.py
+++ ui/streamlit.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Placeholder for Github Actions")
-st.write("How far down the rabbit hole does google cloud docs go")
+st.write("How far down the rabbit hole does google cloud docs go?")
