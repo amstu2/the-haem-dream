7436b004d6ca2c7da0bdc1276fd7cba4b759e940
amstu2 <11323044+amstu2@users.noreply.github.com>
2026-05-08 19:35:41 +1000

    Fix missing bracket

--- src/the_haem_dream/training/densenet.py
+++ src/the_haem_dream/training/densenet.py
@@ -1,19 +1,23 @@
 import os
+from pathlib import Path
+
+import ray
 import torch
 import torch.nn as nn
-from torchvision import transforms
-from torchvision.models import densenet121
-import ray
-from ray.train import ScalingConfig, RunConfig, Checkpoint
-from ray.train.torch import TorchTrainer
-from pathlib import Path
 from ray.data.datasource.partitioning import Partitioning
 from ray.data.preprocessors import LabelEncoder
+from ray.train import Checkpoint, RunConfig, ScalingConfig
+from ray.train.torch import TorchTrainer
+from torchvision import transforms
+from torchvision.models import densenet121
+
 from the_haem_dream.utils import download_gcs_file, unzip_file
 
 ray.init()
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(Path("C:\\Users\\andre\\Downloads\\the-haem-dream-246d90e23cca.json"))
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(
+    Path("C:\\Users\\andre\\Downloads\\the-haem-dream-246d90e23cca.json")
+)
 
 download_gcs_file("the-haem-dream", "cell-dataset/pbc_dataset.zip", "./pbc_dataset.zip")
 download_gcs_file("the-haem-dream", "cell-dataset/pbc_meta.csv", "./pbc_meta.csv")
@@ -29,12 +33,13 @@ train_ds = ray.data.read_images(train_root, size=(368, 368), partitioning=partit
 encoder = LabelEncoder(label_column="class")
 train_ds = encoder.fit_transform(train_ds)
 
-def train_func(config:
+
+def train_func(config):
     data_shard = ray.train.get_dataset_shard("train")
 
     transformations = transforms.Compose(
         [
-            #transforms.ToTensor(),
+            # transforms.ToTensor(),
             transforms.RandomVerticalFlip(p=0.5),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.ColorJitter(brightness=0.2, contrast=0.2),
@@ -42,7 +47,8 @@ def train_func(config:
     )
 
     dataloader = data_shard.iter_torch_batches(
-        batch_size=config["batch_size"], dtypes={"image": torch.uint8, "class": torch.int64}
+        batch_size=config["batch_size"],
+        dtypes={"image": torch.uint8, "class": torch.int64},
     )
 
     model = densenet121(weights=None)
@@ -63,15 +69,15 @@ def train_func(config:
         for batch in dataloader:
             classes = batch["class"]
             images = batch["image"] / 255.0
-            images = images.permute(0, 3, 1, 2) 
-            images = transformations(images) 
-            
+            images = images.permute(0, 3, 1, 2)
+            images = transformations(images)
+
             optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, classes)
             loss.backward()
             optimizer.step()
-        
+
         if epoch % 10 == 0:
             torch.save(model.state_dict(), f"{checkpoint_dir}/model.pth")
             checkpoint = Checkpoint.from_directory(checkpoint_dir)
