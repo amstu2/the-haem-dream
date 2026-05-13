093395710bf2009477f278aacd4594f1c450cb67
amstu2 <11323044+amstu2@users.noreply.github.com>
2026-05-13 20:26:08 +1000

    Rename file to avoid circular import

--- ui/st-app.py
+++ ui/st-app.py
@@ -5,19 +5,20 @@ from PIL import Image
 st.title("Cell Detection")
 
 cell_image = st.file_uploader("Upload cell image", type="image/*")
-
-image = Image.open(cell_image)
-st.image(image, caption="Uploaded Cell Image")
+if cell_image:
+    image = Image.open(cell_image)
+    st.image(image, caption="Uploaded Cell Image")
 
 if st.button("Run Inference"):
-    img_bytes = uploaded_file.getvalue()
+    img_bytes = cell_image.getvalue()
 
     response = requests.post(
-        "http://cell-detector.ray-serve:8000",
+        # "http://cell-detector.ray-serve:8000",
+        "http://localhost:8000",
         files={"file": ("image.jpg", img_bytes, "image/jpeg")},
     )
 
     if response.status_code == 200:
         st.write("Results:", response.json())
     else:
-        st.error(f"ERORR: {respose.content}")
+        st.error(f"ERORR: {response.content}")
