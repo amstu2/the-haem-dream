d3173fb5d107717c0ea469b6806f34c467450a05
amstu2 <11323044+amstu2@users.noreply.github.com>
2026-05-14 18:27:11 +1000

    Switch to FastAPI and add env vars as parameters

--- ray-serve/serve-cell-detector.py
+++ ray-serve/serve-cell-detector.py
@@ -4,15 +4,27 @@ from io import BytesIO
 import mlflow
 import starlette
 import torch
+from fastapi import FastAPI, UploadFile
 from PIL import Image
 from ray import serve
 from torchvision import transforms
 from utils import mlflow_server_alive
 
+app = FastAPI()
 
-@serve.deployment(num_replicas=1, ray_actor_options={"num_cpus": 4})
+NUM_REPLICAS = int(os.environ["NUM_REPLICAS"])
+NUM_CPUS = int(os.environ["NUM_CPUS"])
+NUM_GPUS = int(os.environ["NUM_GPUS"])
+
+
+@serve.deployment(
+    num_replicas=NUM_REPLICAS,
+    ray_actor_options={"num_cpus": NUM_CPUS, "num_gpus": NUM_GPUS},
+)
+@serve.ingress(app)
 class CellDetectionModel:
     def __init__(self):
+        MLFLOW_TRACKING_URI = os.environ["MLFLOW_TRACKING_URI"]
         if not mlflow_server_alive(MLFLOW_TRACKING_URI):
             raise ConnectionError(
                 f"Can't connect to mlflow server ({MLFLOW_TRACKING_URI}- ending..."
@@ -34,7 +46,9 @@ class CellDetectionModel:
             "Segmented Neutrophil": 12,
         }
         self.ID_TO_CLASS_MAP = {v: k for k, v in self.CLASS_TO_ID_MAP.items()}
-        self.model = mlflow.pytorch.load_model(os.environ["MODEL_URI"])
+
+        MODEL_URI = os.environ["CHAMPION_MODEL_URI"]
+        self.model = mlflow.pytorch.load_model(MODEL_URI)
         self.model.eval()
         self.preprocessor = transforms.Compose(
             [
@@ -43,9 +57,10 @@ class CellDetectionModel:
             ]
         )
 
-    async def __call__(self, request: starlette.requests.Request):
-        image_payload_bytes = await request.body()
-        image = Image.open(BytesIO(image_payload_bytes))
+    @app.post("/infer")
+    def infer(self, file: UploadFile):
+        data = file.file.read()
+        image = Image.open(BytesIO(data))
 
         input_tensor = self.preprocessor(image).unsqueeze(0)
         with torch.no_grad():
