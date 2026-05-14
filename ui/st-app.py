0e7a28957ca8ec247ac1b44100de4f99b9ae6baa
amstu2 <11323044+amstu2@users.noreply.github.com>
2026-05-14 18:31:25 +1000

    Parameterise URL and read buff

--- ui/st-app.py
+++ ui/st-app.py
@@ -1,24 +1,33 @@
+import io
+import os
+
 import requests
 import streamlit as st
 from PIL import Image
 
 st.title("Cell Detection")
 
+MODEL_SERVER_URL = os.environ["MODEL_SERVER_URL"]
+
+image = None
 cell_image = st.file_uploader("Upload cell image", type="image/*")
 if cell_image:
     image = Image.open(cell_image)
     st.image(image, caption="Uploaded Cell Image")
 
 if st.button("Run Inference"):
-    img_bytes = cell_image.getvalue()
+    buf = io.BytesIO()
+    if image is not None:
+        image.save(buf, format="jpg")
 
-    response = requests.post(
-        # "http://cell-detector.ray-serve:8000",
-        "http://localhost:8000",
-        files={"file": ("image.jpg", img_bytes, "image/jpeg")},
-    )
+        img_bytes = buf.getvalue()
 
-    if response.status_code == 200:
-        st.write("Results:", response.json())
-    else:
-        st.error(f"ERORR: {response.content}")
+        response = requests.post(
+            MODEL_SERVER_URL,  # "http://cell-detector.ray-serve:8000/infer"
+            files={"file": ("image.jpg", img_bytes, "image/jpeg")},
+        )
+
+        if response.status_code == 200:
+            st.write("Results:", response.json())
+        else:
+            st.error(f"ERORR: {response.content}")
