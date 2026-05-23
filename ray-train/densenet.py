fdedf4e557afdbb8b803838cc601f1c1eec80ddc
amstu2 <11323044+amstu2@users.noreply.github.com>
2026-05-23 14:12:20 +1000

    Add normalisation transform

--- ray-train/densenet.py
+++ ray-train/densenet.py
@@ -93,6 +93,9 @@ def train_func(config):
             transforms.ColorJitter(
                 brightness=config["prob_brightness"], contrast=config["prob_contrast"]
             ),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),  # https://pytorch.org/hub/pytorch_vision_densenet/
         ]
     )
 
